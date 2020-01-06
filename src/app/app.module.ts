608453ee1b148d7d7b112cd8b526f5387af4256b
DuDuSiowski <duduspatryk1@interia.eu>
2020-01-06 20:32:30 +0100

    Adding routing, starting first page module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
+const appRouter=[
+  {
+      path:'',component:MainComponent
+  },
+  {
+      path:'main',component:MainComponent
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
