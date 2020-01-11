8961faed0730521ac255c24fb064600cb8710d7b
DuDuSiowski <duduspatryk1@interia.eu>
2020-01-11 21:56:10 +0100

    Many changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,31 +11,141 @@ export class MainComponent implements OnInit {
 
   error:any; 
   channelInfo: any;
+  value: any;
+
   avatar: any;
+  name: any;
+  subcount: any;
+  channelID: any;
+
+  inserted: boolean;
 
   constructor(private translate: TranslateService, private _youtube_api:YoutubeApiService) {
     translate.setDefaultLang('pl');
    }
 
-
-  async ngOnInit() {
-    // this.value=localStorage.getItem('location');
-    // console.log(this.value);
-    // this.location=JSON.parse(this.value);
-
-    /*
-      Get your current weather JSON from API from getWeather() function in weather.service.
-    */
+   async saveID(channelID: string){
+      this.channelID = channelID;
     try{
-      await this._youtube_api.getChannelInfo("UCZ1MLwA-6jk9zQtBXT5N1Lw").subscribe((response)=>{ //Przekazanie ChannelID
-        if(response==null) this.error="Error, There is no such city or country. Go to settings!";  //Error
-        else this.channelInfo=response["items"]["0"]; //Pobranie JSON
-        console.log(this.channelInfo); //Wyświetlenie JSON
+      await this._youtube_api.getChannelInfoByID(this.channelID).subscribe((response)=>{ //Przekazanie ChannelID
+        if(response==null) this.error="Error, There is no channel, or ID is invalid!";  //Error
+        else{ this.channelInfo=response["items"]["0"]; //Pobranie JSON
+        
         this.avatar = this.channelInfo["snippet"]["thumbnails"]["high"]["url"]; //Pobranie avatara JSON
+        this.name = this.channelInfo["snippet"]["title"];
+        console.log(this.channelInfo["statistics"]["subscriberCount"]);
+        this.subcount = this.channelInfo["statistics"]["subscriberCount"];
+        this.inserted=true;
+        localStorage.setItem("youtuberID", JSON.stringify(this.channelID));
+      }
       });
     } catch(error){
-      //await this.handleError(error);
-      this.error="Error, There is no such city or country";
+      this.error="Error, There is no channel, or ID is invalid!";
     }
+    
+   }
+  async ngOnInit() {
+     this.value=localStorage.getItem('youtuberID');
+     console.log(this.value);
+     this.channelID=JSON.parse(this.value);
+     console.log(this.channelID);
+    /*
+      Get your channels info and get JSON from API   
+      
+    */
+    if(this.channelInfo != null || this.channelInfo != ""){
+      try{
+        await this._youtube_api.getChannelInfoByID(this.channelID).subscribe((response)=>{ //Przekazanie ChannelID
+          if(response==null) this.error="Error, There is no channel, or ID is invalid!";  //Error
+          else{ this.channelInfo=response["items"]["0"]; //Pobranie JSON
+          
+          this.avatar = this.channelInfo["snippet"]["thumbnails"]["high"]["url"]; //Pobranie avatara JSON
+          this.name = this.channelInfo["snippet"]["title"];
+          console.log(this.channelInfo["statistics"]["subscriberCount"]);
+          this.subcount = this.channelInfo["statistics"]["subscriberCount"];
+          this.inserted=true;
+        }
+        });
+      } catch(error){
+        this.error="Error, There is no channel, or ID is invalid!";
+      }
+    }
+
+   //PewDiePie info from API
+    this.getPewDiePie();
+
+    //Kuba Klawier info from API 
+    this.getKubaKlawier();
+
+    //TVNTurbo info from API 
+    this.getTVNTurbo();
+}
+
+//PewDiePie
+avatarPewDiePie: any;
+descriptionPewDiePie: any;
+namePewDiePie: any;
+idPewDiePie: any;
+
+async getPewDiePie(){
+   //PewDiePie info from API
+   this.idPewDiePie = "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+    try{
+      await this._youtube_api.getChannelInfoByID(this.idPewDiePie).subscribe((response)=>{ //Przekazanie ChannelID
+        if(response==null) this.error="Error, There is no channel, or ID is invalid!";  //Error
+        else this.channelInfo=response["items"]["0"]; //Pobranie JSON
+        this.avatarPewDiePie = this.channelInfo["snippet"]["thumbnails"]["high"]["url"]; //Pobranie avatara JSON
+        this.namePewDiePie = this.channelInfo["snippet"]["title"];
+        this.descriptionPewDiePie = this.channelInfo["snippet"]["description"];
+
+      });
+    } catch(error){
+      this.error="Error, There is no channel, or ID is invalid!";
+    }
+}
+
+//Kuba Klawier
+avatarKuba: any;
+descriptionKuba: any;
+nameKuba: any;
+idKuba: any;
+
+async getKubaKlawier(){
+  try{
+    this.idKuba="UCLr4hMhk_2KE0GUBSBrspGA";
+    await this._youtube_api.getChannelInfoByID(this.idKuba).subscribe((response)=>{ //Przekazanie ChannelID
+      if(response==null) this.error="Error, There is no channel, or ID is invalid!";  //Error
+      else this.channelInfo=response["items"]["0"]; //Pobranie JSON
+      this.avatarKuba = this.channelInfo["snippet"]["thumbnails"]["high"]["url"]; //Pobranie avatara JSON
+      this.nameKuba = this.channelInfo["snippet"]["title"];
+      this.descriptionKuba = this.channelInfo["snippet"]["description"];
+
+    });
+  } catch(error){
+    this.error="Error, There is no channel, or ID is invalid!";
+  }
+}
+
+//TVN Turbo
+avatarTVN: any;
+descriptionTVN: any;
+nameTVN: any;
+idTVN: any;
+
+async getTVNTurbo(){
+  try{
+    this.idTVN="UCfsSoUSQzUimrjKhAKmshDQ";
+    await this._youtube_api.getChannelInfoByID(this.idTVN).subscribe((response)=>{ //Przekazanie ChannelID
+      if(response==null) this.error="Error, There is no channel, or ID is invalid!";  //Error
+      else this.channelInfo=response["items"]["0"]; //Pobranie JSON
+      this.avatarTVN = this.channelInfo["snippet"]["thumbnails"]["high"]["url"]; //Pobranie avatara JSON
+      this.nameTVN = this.channelInfo["snippet"]["title"];
+      this.descriptionTVN = this.channelInfo["snippet"]["description"];
+
+    });
+  } catch(error){
+    //await this.handleError(error);
+    this.error="Error, There is no channel, or ID is invalid!";
+  }
 }
 }
--- src/app/youtube-api.service.ts
+++ src/app/youtube-api.service.ts
@@ -5,9 +5,12 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class YoutubeApiService {
-  channelInfo = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=";
+  channelInfoByID = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=";
+  channelChannelIDInfo = "https://www.googleapis.com/youtube/v3/channels?part=id&forUsername="
   channelId;
+  channelName;
   API_KEY = "AIzaSyD69125P66Ki46g0fLue8b3YQrBvur_wAI";
+  error:any; 
 
   constructor(private http:HttpClient) { }
 
@@ -15,8 +18,26 @@ export class YoutubeApiService {
   //   this.channelId=channelId;
   // }
 
-  getChannelInfo(channelId){
-    return this.http.get(this.channelInfo+channelId+"&key="+this.API_KEY);
+  getChannelInfoByID(channelId){
+    this.channelId = channelId;
+    return this.http.get(this.channelInfoByID+this.channelId+"&key="+this.API_KEY);
   }
+
+  async getChannelID(channelName){
+      try{
+          await this.http.get(this.channelChannelIDInfo+channelName+"&key="+this.API_KEY).subscribe((response)=>{ //Przekazanie ChannelID
+          if(response==null) this.error="Error, no informations!";  //Error
+          else this.channelId=response["items"]["0"]["id"]; //Pobranie JSON
+          console.log(this.channelId); //Wyświetlenie JSON
+          return  this.http.get(this.channelInfoByID+this.channelId+"&key="+this.API_KEY);
+      });
+    } catch(error){
+          //await this.handleError(error);
+          this.error="Error, There is no channel on this username";
+          console.log(this.error);
+    }
+  }
+
+  
 
 }
